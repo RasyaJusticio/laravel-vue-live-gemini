3d64d08ea6e805d00ed72840954c1fd1cbc8e042
RasyaJusticio <rasyajusticio.personal@gmail.com>
2025-08-08 11:41:28 +0700

    feat: integrate Gemini Live API with audio recording

--- resources/js/components/MainApp.vue
+++ resources/js/components/MainApp.vue
@@ -1,22 +1,73 @@
 <script setup lang="ts">
-import { Mic, Video } from 'lucide-vue-next';
+import { Mic, MicOff, Video } from 'lucide-vue-next';
+import { useLiveAPIContext } from './LiveAPIContext';
+import { computed, onUnmounted, ref, watch } from 'vue';
+import { AudioRecorder } from '@/lib/audio-recorder';
+
+const liveAPI = useLiveAPIContext();
+
+const client = computed(() => liveAPI.client);
+const connected = computed(() => liveAPI.connected.value);
+const isTalking = computed(() => liveAPI.isTalking.value);
+
+const audioRecorder = ref<AudioRecorder>(new AudioRecorder());
+const volume = ref<number>(0);
+const muted = ref<boolean>(false);
+
+const onAudioRecorderData = (base64: string) => {
+    if (muted.value || isTalking.value) {
+        return;
+    }
+
+    client.value.sendRealtimeInput([
+        {
+            mimeType: "audio/pcm;rate=16000",
+            data: base64,
+        },
+    ]);
+}
+
+const setInVolume = (amount: number) => {
+    volume.value = volume.value + amount;
+}
+
+const setMuted = (value: boolean) => {
+    muted.value = value;
+}
+
+watch(connected, () => {
+    if (connected.value) {
+        audioRecorder.value.on("data", onAudioRecorderData)
+        audioRecorder.value.on("volume", setInVolume);
+        audioRecorder.value.start();
+    } else {
+        audioRecorder.value.stop();
+    }
+});
+
+onUnmounted(() => {
+    audioRecorder.value.off("data", onAudioRecorderData)
+    audioRecorder.value.off("volume", setInVolume);
+});
 
 </script>
 
 <template>
     <div class="relative w-full h-full border-2 border-blue-950 rounded-xl">
-        <button class="absolute rounded-xl bg-blue-700 border border-blue-700 text-white cursor-pointer outline-0 p-3 hover:bg-transparent hover:text-blue-700 transition-colors disabled:opacity-50 disabled:pointer-events-none top-4 left-1/2 -translate-x-1/2">
-            Connect
+        <button
+            class="absolute rounded-xl bg-blue-700 border border-blue-700 text-white cursor-pointer outline-0 p-3 hover:bg-transparent hover:text-blue-700 transition-colors disabled:opacity-50 disabled:pointer-events-none top-4 left-1/2 -translate-x-1/2"
+            @click="connected ? liveAPI.disconnect() : liveAPI.connect()">
+            {{ connected ? "Disconnect" : "Connect" }}
         </button>
         <div class="absolute flex gap-2 left-1/2 -translate-1/2 bottom-2 border-blue-950 rounded-xl border-2 p-2">
             <button
                 class="rounded-xl bg-blue-700 border border-blue-700 text-white cursor-pointer outline-0 p-3 hover:bg-transparent hover:text-blue-700 transition-colors disabled:opacity-50 disabled:pointer-events-none"
-                >
-                <Mic />
+                @click="muted ? setMuted(false) : setMuted(true)">
+                <MicOff v-if="muted" />
+                <Mic v-else />
             </button>
             <button
-                class="rounded-xl bg-blue-700 border border-blue-700 text-white cursor-pointer outline-0 p-3 hover:bg-transparent hover:text-blue-700 transition-colors disabled:opacity-50 disabled:pointer-events-none"
-                >
+                class="rounded-xl bg-blue-700 border border-blue-700 text-white cursor-pointer outline-0 p-3 hover:bg-transparent hover:text-blue-700 transition-colors disabled:opacity-50 disabled:pointer-events-none">
                 <Video />
             </button>
         </div>
--- resources/js/pages/App.vue
+++ resources/js/pages/App.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
+import { LiveAPIProvider } from '@/components/LiveAPIContext';
 import MainApp from '@/components/MainApp.vue';
+import { LiveClientOptions } from '@/types/genai';
 import { Head } from '@inertiajs/vue3';
+
+const options: LiveClientOptions = {
+    apiKey: import.meta.env.VITE_GEMINI_API_KEY || "",
+}
+
 </script>
 
 <template>
 
     <Head title="Laravel Vue Live Gemini"></Head>
     <div class="flex h-screen flex-col items-center bg-blue-100 p-6 text-[#1b1b18] lg:justify-center lg:p-8">
-        <MainApp />
+        <LiveAPIProvider :options="options">
+            <MainApp />
+        </LiveAPIProvider>
     </div>
 </template>
